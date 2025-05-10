c69c96cc396e71ba0cf1ab1342718e05e3ef4152
Huan <thuhiendinhhuan@gmail.com>
2025-05-10 09:43:52 +0700

    data ingestion and transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,9 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split
+
+from src.components.data_transformation import DataTransformation
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join('artifacts', 'train.csv')
@@ -17,7 +20,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Starting data ingestion")
         try:
-            df = pd.read_csv('data/stud.csv')
+            df = pd.read_csv(rf'C:\Users\thuhi\pythonlearn\mlproject\data\stud.csv')
             logging.info("Read the dataset as dataframe")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
@@ -36,4 +39,6 @@ class DataIngestion:
             
 if __name__ == "__main__":
     ingestor  = DataIngestion()
-    ingestor.initiate_data_ingestion()
+    train_data,test_data = ingestor.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
---
+++ src/components/data_transformation.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import pandas as pd
+from dataclasses import dataclass
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+import numpy as np
+from src.utils import save_object
+import pickle
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
+    transformed_train_file_path = os.path.join('artifacts', 'train_transformed.csv')
+    transformed_test_file_path = os.path.join('artifacts', 'test_transformed.csv')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+    def get_data_transformer_object(self,numerical_features, categorical_features):
+        try:
+            logging.info("Data transformation initiated")
+            num_transformer = Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy = 'median')),
+                    ('scaler',StandardScaler())
+                ]
+            )
+            cat_transformer = Pipeline(
+                steps = [
+                    ('imputer',SimpleImputer(strategy='most_frequent'))
+                ]
+            )
+
+            preprocessor = ColumnTransformer(
+                transformers=[
+                    ('num', num_transformer,numerical_features),
+                    ('cat',cat_transformer,categorical_features)
+                ]
+            )
+            logging.info("Saving preprocessor object")
+            return preprocessor
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+    def initiate_data_transformation(self,train_file_path,test_file_path):
+        numerical_features = ['writing_score', 'reading_score']
+        categorical_features = [
+                'gender',
+                'race_ethnicity',
+                'parental_level_of_education',
+                'lunch',
+                'test_preparation_course'
+            ]
+            
+        try:
+            df_train = pd.read_csv(train_file_path)
+            df_test = pd.read_csv(test_file_path)   
+            logging.info('Read train and test data')
+            logging.info('Obtaining preprocessing object')
+            preprocessing_obj = self.get_data_transformer_object(numerical_features, categorical_features)
+            target_column_name = 'math_score'
+            input_features_train = df_train.drop(columns =[target_column_name])
+            target_feature_train = df_train[target_column_name]
+            input_feature_test = df_test.drop(columns =[target_column_name])
+            target_feature_test = df_test[target_column_name]
+            logging.info('Applying preprocessing object on training and testing data')
+            
+            input_features_trained = preprocessing_obj.fit_transform(input_features_train)
+            input_features_tested = preprocessing_obj.transform(input_feature_test)
+            train_arr = np.concatenate([input_features_trained, target_feature_train.to_numpy().reshape(-1, 1)], axis=1)
+            test_arr = np.concatenate([input_features_tested, target_feature_test.to_numpy().reshape(-1, 1)], axis=1)
+            logging.info("Saving processed objects")
+            pd.DataFrame(train_arr).to_csv(self.data_transformation_config.transformed_train_file_path, index=False, header=True)
+            pd.DataFrame(test_arr).to_csv(self.data_transformation_config.transformed_test_file_path, index=False, header=True)
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+           
+        except Exception as e:
+            raise CustomException(e, sys)
+        
